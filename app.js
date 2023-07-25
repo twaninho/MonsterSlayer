f022780dc849d91c4054507cccefd7e85051ec4f
Twan Hopmans <twanjhopmans@hotmail.com>
2023-07-25 20:01:14 +0200

    attack monster and player methods

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
   },
 
   computed: {},
 
-  methods: {},
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth = this.playerHealth - attackValue;
+    },
+  },
 });
 
 app.mount("#game");
