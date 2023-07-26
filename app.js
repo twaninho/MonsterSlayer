61d83f9d38a08baf4abceb4946b0b2a7d927338c
Twan Hopmans <twanjhopmans@hotmail.com>
2023-07-26 17:22:24 +0200

    adding messages and colors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
 
@@ -37,17 +38,20 @@ const app = Vue.createApp({
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth = this.monsterHealth - attackValue;
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth = this.playerHealth - attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
     },
 
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth = this.monsterHealth - attackValue;
+      this.addLogMessage("player", "special-attack", attackValue);
       this.attackPlayer();
     },
 
@@ -59,6 +63,7 @@ const app = Vue.createApp({
       } else {
         this.playerHealth = this.playerHealth + healValue;
       }
+      this.addLogMessage("player", "heal", healValue);
       this.attackPlayer();
     },
 
@@ -67,10 +72,19 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.winner = null;
+      this.logMessages = [];
     },
 
     surrender() {
       this.winner = "monster";
+    },
+
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 
